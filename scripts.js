32ba23571926644f307f5b1bbdc2084942f22ae0
Huszár Hunor Zoltán <huszar.hunor@gmail.com>
2022-05-16 23:00:01 +0200

    Alap oldal style elkészítése lap tetejére ugrás gomb berakása

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-var oldalnav = document.getElementById("oldalnav");
+var oldalnav = document.getElementById("laptetejeregomb");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+  if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
     oldalnav.style.display = "block";
   } else {
     oldalnav.style.display = "none";
