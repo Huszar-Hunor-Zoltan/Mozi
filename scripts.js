cc0018cbb64ad68ad23cc6651b4a5f197e0e747b
Huszár Hunor Zoltán <huszar.hunor@gmail.com>
2022-05-18 23:40:51 +0200

    Oldalak majdnem befejezve

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var oldalnav = document.getElementById("laptetejeregomb");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+  if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
     oldalnav.style.display = "block";
   } else {
     oldalnav.style.display = "none";
